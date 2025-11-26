4e52142fc4a312c460e96be5b77e1de2dcb54c7f
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2025-11-26 21:05:32 +0900

    멘트 자동 스크롤 기능 추가

--- js/script.js
+++ js/script.js
@@ -25,14 +25,14 @@ along with this program. If not, see <http://www.gnu.org/licenses/>
 // - 1번 음성 파일(1.mp3) 재생 예시 : https://eh.aplx.link/?id=1
 // 
 const DEF_VOICE_FILE_EXT                = ".mp3";               // 음성 파일의 확장자
-const DEF_VOICE_FILE_PATH               = "/voice/main/";        // 설명 멘트 음성 파일 경로
-const DEF_VOICE_START_PATH         = "/voice/start/";       // 시작 멘트 음성 파일 경로
-const DEF_VOICE_END_PATH           = "/voice/end/";         // 끝맺음 멘트 음성 파일 경로
+const DEF_VOICE_FILE_PATH               = "voice/main/";        // 설명 멘트 음성 파일 경로
+const DEF_VOICE_START_PATH         = "voice/start/";       // 시작 멘트 음성 파일 경로
+const DEF_VOICE_END_PATH           = "voice/end/";         // 끝맺음 멘트 음성 파일 경로
 
 //  ment/ 폴더에 [0 ~ n].html 이름의 텍스트 설명 파일들이 있습니다.
 //  [n]값에 해당하는 html파일을 읽어와 표시합니다.
 //
-const DEF_TEXT_MAIN_PATH                = "/ment/";              // 설명 텍스트를 포함하는 HTML 파일 경로
+const DEF_TEXT_MAIN_PATH                = "ment/";              // 설명 텍스트를 포함하는 HTML 파일 경로
 
 //  voice/start 폴더에 시작 멘트 파일들이 존재하고 [id].mp3 파일을 임의로 선택해서 재생합니다.
 //
@@ -54,6 +54,8 @@ let isByStopButton                  = false;
 
 let endCallback                     = null;
 let canPlayCallback                 = null;
+
+let mentDuration = 0;
 
 const audioSource = document.getElementById('audioSource');
 const audioElement = document.querySelector("audio");
@@ -106,6 +108,22 @@ function setEndMent() {
     });
 }
 
+function mentScroll(isHow) {
+    if (isHow) {
+        $('.body').animate({
+            scrollTop: 0
+        }, 50);
+
+        var targetOffset = $('.body')[0].scrollHeight; // Get the position relative to the parent div                        
+        $('.body').animate({
+            scrollTop: targetOffset
+        }, mentDuration * 1500);
+    }
+    else {
+        $('.body').stop(true, false);
+    }
+}
+
 function realContentPlay(id) {
     if (id < 0) return;
 
@@ -115,11 +133,13 @@ function realContentPlay(id) {
             setEndMent();
         }, 
         function() {
+            mentDuration = audioElement.duration;            
+            audioElement.play();
+            mentScroll(true);
             $("#playButton").hide();
             $("#stopButton").show();
             $("#pauseButton").show();
             showLoaderAni();
-            audioElement.play();
             isPlay = true;
             isPaused = false;
             isByStopButton = false;
@@ -160,7 +180,7 @@ function setCurAudio(id, fendCallback, fcanPlayCallback) {
     if (audioCtx.state === "suspended") {
         audioCtx.resume();
     }
-        
+            
     endCallback = fendCallback;
     canPlayCallback = fcanPlayCallback;
 }
@@ -181,6 +201,10 @@ function init() {
     $("#qrScanBtn").click(function() {
         GA_EVENT("qrScanBtn", "click", "service");
         vibrate();
+    });
+
+    $(".summary").on('touchstart', function() {
+        mentScroll(false);
     });
     
     $("#playButton").click(function() {
@@ -210,6 +234,7 @@ function init() {
 
         audioElement.pause();
         audioElement.currentTime = 0;
+        mentScroll(false);
 
         $("#playButton").show();
         $("#stopButton").hide();
@@ -222,7 +247,9 @@ function init() {
         vibrate();
 
         isPaused = true;
-        audioElement.pause();        
+        audioElement.pause();
+        mentScroll(false);
+
         $("#pauseButton").hide();
         $("#playButton").show();
         hideLoaderAni();
